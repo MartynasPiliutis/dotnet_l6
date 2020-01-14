f6e69444b84899d0d79c0c5d2aced573ba74bc8b
MartynasPiliutis <46004570+MartynasPiliutis@users.noreply.github.com>
2020-01-14 20:48:55 +0200

    l6 t3 complete. L6T4 created

---
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pradineEilute = "Karabasas";
+            string eilute = sukuriaEilute(pradineEilute);
+            Console.WriteLine(eilute);
+            Console.ReadLine();
+        }
+
+        static string sukuriaEilute(string pradineEilute)
+        {
+            int ilgis = pradineEilute.Length;
+            string sharpuEilute = "";
+            for (int i = 0; i < ilgis; i++)
+            {
+                sharpuEilute += "#";
+            }
+            return sharpuEilute;
+        }
+
+    }
+}
