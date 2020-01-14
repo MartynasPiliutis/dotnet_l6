ad3d3433612b02f1f459bb90715ded7a9363aa2e
MartynasPiliutis <46004570+MartynasPiliutis@users.noreply.github.com>
2020-01-14 20:18:12 +0200

    l6 t2 complete

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int repeat = 10;
+            string vardas = "Name";
+
+            isvedaVardaKonsole(repeat, vardas);
+            Console.ReadLine();
+        }
+
+        private static void isvedaVardaKonsole(int repeat, string vardas)
+        {
+            for (int i = 0; i < repeat; i++)
+            {
+                Console.WriteLine(vardas);
+            }
+        }
+    }
+}
